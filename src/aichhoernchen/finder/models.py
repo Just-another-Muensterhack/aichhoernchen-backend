7d073074ba6d7d6d6bf2984eadae4afe58cb5579
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-09-26 12:29:38 +0000

    wip: add models

--- src/aichhoernchen/finder/models.py
+++ src/aichhoernchen/finder/models.py
@@ -5,11 +5,25 @@ class FoundObject(models.Model):
     short_title = models.TextField()
     long_title = models.TextField()
     description = models.TextField()
-    lat = models.DecimalField()
-    long = models.DecimalField()
-    timestampt = models.DateTimeField(auto_now_add=True)
-    finder = models.ForeignKey("Finder", on_delete=models.CASCADE)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    long = models.DecimalField(max_digits=9, decimal_places=6)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    finder = models.ForeignKey("Finder", on_delete=models.SET_NULL, null=True, blank=True)
+    deposit = models.ForeignKey("LostPropertyOffice", on_delete=models.SET_NULL, null=True, blank=True)
+
 
 
 class Finder(models.Model):
     name = models.TextField()
+    email = models.EmailField()
+    phone = models.TextField()
+
+
+class LostPropertyOffice(models.Model):
+    name = models.TextField()
+    email = models.EmailField()
+    phone = models.TextField()
+    address = models.TextField()
+    link = models.TextField()
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    long = models.DecimalField(max_digits=9, decimal_places=6)
