7b72048036e0dc99687b34e8557b8ae61676e13c
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-09-26 13:58:03 +0000

    wip: add cd and api

---
+++ src/aichhoernchen/finder/schema.py
@@ -0,0 +1,23 @@
+import strawberry
+
+from .types import FinderType, FoundObjectType, LostPropertyOfficeType
+
+
+@strawberry.type
+class Query:
+    found_object: FoundObjectType = strawberry.field()
+    found_objects: list[FoundObjectType] = strawberry.field()
+    finder: FinderType = strawberry.field()
+    finders: list[FinderType] = strawberry.field()
+
+    lost_property_office: LostPropertyOfficeType = strawberry.field()
+    lost_property_offices: list[LostPropertyOfficeType] = strawberry.field()
+
+
+
+@strawberry.type
+class Mutation:
+    pass
+
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
---
+++ src/aichhoernchen/finder/types.py
@@ -0,0 +1,113 @@
+from __future__ import annotations
+
+from typing import Optional
+
+from strawberry import auto
+from strawberry.django import filters, order_type
+from strawberry.django import type as graphql_type
+
+from .models import Finder, FoundObject, LostPropertyOffice
+
+
+# filters
+@filters.filter_type(FoundObject)
+class FoundObjectFilter:
+    short_title: auto
+    long_title: auto
+    description: auto
+    lat: auto
+    long: auto
+    timestamp: auto
+    finder: Optional[FinderFilter]
+    deposit: Optional[LostPropertyOfficeFilter]
+
+
+@filters.filter_type(Finder)
+class FinderFilter:
+    name: auto
+    email: auto
+    phone: auto
+    found_objects: Optional[FoundObjectFilter]
+
+
+@filters.filter_type(LostPropertyOffice)
+class LostPropertyOfficeFilter:
+    name: auto
+    email: auto
+    phone: auto
+    address: auto
+    link: auto
+    lat: auto
+    long: auto
+    found_objects: Optional[FoundObjectFilter]
+
+
+# ordering
+@order_type(FoundObject)
+class FoundObjectOrder:
+    id: auto
+    short_title: auto
+    long_title: auto
+    description: auto
+    lat: auto
+    long: auto
+    timestamp: auto
+
+
+
+@order_type(Finder)
+class FinderOrder:
+    id: auto
+    name: auto
+    email: auto
+    phone: auto
+    found_object: FoundObjectOrder
+
+
+@order_type(LostPropertyOffice)
+class LostPropertyOfficeOrder:
+    id: auto
+    name: auto
+    email: auto
+    phone: auto
+    address: auto
+    link: auto
+    lat: auto
+    long: auto
+    found_objects: Optional[FoundObjectOrder]
+
+
+# types
+@graphql_type(FoundObject)
+class FoundObjectType:
+    id: auto
+    short_title: auto
+    long_title: auto
+    description: auto
+    lat: auto
+    long: auto
+    timestamp: auto
+    finder: FinderType
+    deposit: Optional[LostPropertyOfficeType]
+
+
+@graphql_type(Finder)
+class FinderType:
+    id: auto
+    name: auto
+    email: auto
+    phone: auto
+    found_object: FoundObject
+
+
+@graphql_type(LostPropertyOffice)
+class LostPropertyOfficeType:
+    id: auto
+    name: auto
+    address: auto
+    phone: auto
+    email: auto
+    link: auto
+    lat: auto
+    long: auto
+    found_objects: list[FoundObject]
