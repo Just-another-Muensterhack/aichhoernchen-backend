30143ed9ea70b505001de9790fa1236fae26d395
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-09-26 12:24:58 +0000

    wip: add models

--- src/aichhoernchen/aichhoernchen/urls.py
+++ src/aichhoernchen/aichhoernchen/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# from aichhoernchen.finder.schema import schema
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+#    path("graphql/", csrf_exempt(GraphQLView.as_view(schema=schema))),
 ]
--- src/aichhoernchen/finder/models.py
+++ src/aichhoernchen/finder/models.py
@@ -1,2 +1,15 @@
+from django.db import models
 
-# Create your models here.
+
+class FoundObject(models.Model):
+    short_title = models.TextField()
+    long_title = models.TextField()
+    description = models.TextField()
+    lat = models.DecimalField()
+    long = models.DecimalField()
+    timestampt = models.DateTimeField(auto_now_add=True)
+    finder = models.ForeignKey("Finder", on_delete=models.CASCADE)
+
+
+class Finder(models.Model):
+    name = models.TextField()
